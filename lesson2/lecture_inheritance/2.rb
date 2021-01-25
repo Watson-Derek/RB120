fd5289da424b0caf668b87c89eff9def98144539
Derek Watson <watso128@gmail.com>
2021-01-25 18:02:39 -0500

    committing two sections of exercises and more files from lesson 2 lectures

---
+++ lesson2/lecture_inheritance/2.rb
@@ -0,0 +1,31 @@
+class Dog
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+
+  def run
+    'running!'
+  end
+
+  def jump
+    'jumping!'
+  end
+
+  def fetch
+    'fetching!'
+  end
+end
+
+class Cat < Dog
+  def swim
+    "can't swim!"
+  end
+
+  def fetch
+    "can't fetch!"
+  end
+end
